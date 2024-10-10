56be97ed1928b045bbf906bae58502d8cab68deb
JoxaUzKing <joxauzking@gmail.com>
2024-10-11 01:02:21 +0500

    exam homework

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 window.addEventListener('scroll', function() {
     const header = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
+    if (window.scrollY > 22) {
         header.classList.add('scrolled');
     } else {
         header.classList.remove('scrolled');
